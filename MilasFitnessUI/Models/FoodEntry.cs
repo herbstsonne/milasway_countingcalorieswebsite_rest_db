a48dd03a33f0f818f3802730585d77f03a2320e2
Mila <57719825+herbstsonne@users.noreply.github.com>
2020-05-21 22:33:12 +0200

    fixed an error of usinf the inputselect + added sumcalories functionality

--- MilasFitnessUI/Models/FoodEntry.cs
+++ MilasFitnessUI/Models/FoodEntry.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace MilasFitnessUI.Models
 {
     public class FoodEntry
     {
         public Food Food { get; set; }
         public int Amount { get; set; }
+        public int Calories { get; set; }
     }
 }

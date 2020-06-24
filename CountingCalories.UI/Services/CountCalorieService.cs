30d51c365fd6057e8be41248c3f61510571bee10
mila <mirisasse@gmail.com>
2020-06-24 13:58:42 +0200

    removed unnecessary namespaces

--- CountingCalories.UI/Services/CountCalorieService.cs
+++ CountingCalories.UI/Services/CountCalorieService.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
 using CountingCalories.UI.Models;
-using Microsoft.AspNetCore.Components;
-using Newtonsoft.Json;
 
 namespace CountingCalories.UI.Services
 {
